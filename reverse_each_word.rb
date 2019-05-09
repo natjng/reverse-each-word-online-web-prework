dcd60d2b0540143f89e9946639b59324207bf672
Natalie J <natsetc@gmail.com>
2019-05-09 19:18:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   #sentence.to_a --> reverse --> sentence.join(" ")
-  sentence.split
-  sentence.collect {|word| word.reverse}
-  sentence.join(" ")
+  array = sentence.split
+  array.collect {|word| word.reverse}
+  array.join(" ")
 end
